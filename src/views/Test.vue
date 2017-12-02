89d5d8ab67b6f48c941e8783e8c848655b14edf9
Shakro69 <shakro69@gmail.com>
2017-12-02 19:57:06 +0200

    updated

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,19 +1,63 @@
 <template>
   <div id="demo">
-    <button v-on:click="show = !show">
-      Переключить
-    </button>
     <transition name="fade">
-      <p v-if="show">hello</p>
+      <div v-if="show">
+        <h1>Test</h1>
+      <p>{{  message }}</p>
+      <input type="text" v-model="message">
+      <!-- <input type="text" v-model="search" autofocus> -->
+      <!-- <pre>{{ $data }}</pre> -->
+      </div>
     </transition>
+    <div>
+      <ul>
+        <li v-for="bear in bears" v-bind:class="{'strike': bear.status}">Age:{{ bear.age }} - {{ bear.name }}</li>
+      </ul>
+    </div>
+    <button v-on:click.prevent="show = !show">
+      Show/Hide
+    </button>
+
+    <form action="">
+      <button v-on:click="onClick">Test On Click</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default{
   data: () => ({
-    el: '#demo',
-    show: true
-  })
+    bears:[
+      {
+        name: 'brown',
+        status: true,
+        age:18
+      },
+      {
+        name: 'grizli',
+        status: false,
+        age:5
+      },
+      {
+        name: 'panda',
+        status: false,
+        age:7
+      },
+      {
+        name:   'white',
+        status: true,
+        age:12
+      }
+    ],
+    // el: '#demo',
+    show: true,
+    message: 'Some message!!'
+  }),
+  methods:{
+    onClick: function(){
+      // e.preventDefault
+      console.log('Test On Click')
+    }
+  }
 }
 </script>
